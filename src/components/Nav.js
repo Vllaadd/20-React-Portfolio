ed619cf794af30098213eb1172f25dcb3d0ef9e7
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-12-21 04:34:27 -0500

    working on styling the banner

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,17 +6,19 @@ const Nav = () => {
         <nav className='navbar'>
             <div className='container'>
                 <div className='navbar_container'>
-                    <ul className='navbar_left-logo'>
-                        {/* <img src='/images/logo.png' alt='logo' /> */}
+{/*=== NAVBAR LEFT ==================================================== */}
+                    <ul className='navbar_left'>
+                        <div className='navbar_left-logo'>
+                            <img src='/images/logo.png' alt='logo' />
+                        </div>
                     </ul>
-                    {/* {state ? ( */}
-                    <ul className='navbar-right'>
+{/* === NAVBAR RIGHT ================================================== */}
+                    <ul className='navbar_right'>
                         <li><a href='#home'>Home</a></li>
                         <li><a href='#about'>About</a></li>
                         <li><a href='#portfolio'>Portfolio</a></li>
                         <li><a href='#contact'>Contact</a></li>
                     </ul>
-                    {/* ) : ('')} */}
                 </div>
 
             </div>
