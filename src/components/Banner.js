469fc7c98f5f42131cc11430b0918f0c19ff91dc
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-12-21 01:27:29 -0500

    working on styling navbar

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,18 @@ import React from 'react';
 const Banner = () =>{
     return (
         <header className='header'>
-            Some text goes here. 
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-6'>
+                        <div className='header_content'>
+                            <ul className='header_ul'>
+                                <li></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </header>
     );
 }; 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,18 +5,21 @@ const Nav = () => {
     return ( 
         <nav className='navbar'>
             <div className='container'>
-                    <ul className='navbar-left'>
-                        <p>logo goes here</p>
+                <div className='navbar_container'>
+                    <ul className='navbar_left-logo'>
+                        {/* <img src='/images/logo.png' alt='logo' /> */}
                     </ul>
-                    {state ? (
-                        <ul className='navbar-right'>
-                            <li><a href='#home'>Home</a></li>
-                            <li><a href='#about'>About</a></li>
-                            <li><a href='#portfolio'>Portfolio</a></li>
-                            <li><a href='#contact'>Contact</a></li>
-                        </ul>
-                    ) : ('')}
+                    {/* {state ? ( */}
+                    <ul className='navbar-right'>
+                        <li><a href='#home'>Home</a></li>
+                        <li><a href='#about'>About</a></li>
+                        <li><a href='#portfolio'>Portfolio</a></li>
+                        <li><a href='#contact'>Contact</a></li>
+                    </ul>
+                    {/* ) : ('')} */}
                 </div>
+
+            </div>
         </nav>
      );
 };
