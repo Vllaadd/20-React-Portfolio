f53de532848b1c26ffea85490e6a616b047a0b11
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-12-27 10:41:55 -0500

    links added to the social network icons

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -17,10 +17,10 @@ const Banner = () => {
                                 <h1>I am Vlad Zizic</h1>
                                 <p>I am Vladimir Zizic, a professional web developer.</p>
                                 <ul className='header_ul'>
-                                    <li><FaFacebookSquare /></li>
-                                    <li><FaInstagramSquare /></li>
-                                    <li><FaTwitterSquare /></li>
-                                    <li><FaLinkedin /></li>
+                                    <li><a href='https://www.facebook.com/vladazizic' target='_blank'><FaFacebookSquare /></a></li>
+                                    <li><a href='https://www.instagram.com/z.vladd/'><FaInstagramSquare /></a></li>
+                                    <li><a href='https://twitter.com/zVladd'><FaTwitterSquare /></a></li>
+                                    <li><a href='https://www.linkedin.com/in/vladimir-zizic-bb3756134/'><FaLinkedin /></a></li>
                                 </ul>
                             </div>
                         </div>
